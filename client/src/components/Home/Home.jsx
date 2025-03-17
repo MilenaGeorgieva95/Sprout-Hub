c27701760f632c13ecd1357f4a6709da9d8eede7
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-17 01:44:05 +0000

    style home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,10 @@
+import styles from "./Home.module.css";
+
 export default function Home() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div
+      className={"relative isolate px-6 pt-14 lg:px-8 " + styles.heroContainer}
+    >
       <div
         aria-hidden="true"
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
