7a222df2aa8602246ed668f80fd84b94ef7e3c56
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-18 23:01:52 +0000

    add posts details functionality and load data dynamically

--- client/src/components/forum/ForumPosts.jsx
+++ client/src/components/forum/ForumPosts.jsx
@@ -16,7 +16,7 @@ export default function ForumPosts({ posts }) {
           >
             <img
               alt="plant"
-              src="https://images.unsplash.com/photo-1563127673-b35a42ef206c?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={post.imgUrl}
               className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
             />
             <div className="mt-4 flex justify-between">
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 
-export default function useFetch(url, defaultPosts = []) {
+export default function useFetch(url, defaultPosts = [], id) {
   const [state, setState] = useState(defaultPosts);
   const [pending, setPending] = useState(true);
   useEffect(() => {
     setPending(true);
     const abortController = new AbortController();
+    if (id) {
+      url = url + id;
+    }
 
     fetch(url, { signal: abortController.signal })
       .then((res) => res.json())
       .then((result) => {
-        setState(Object.values(result));
+        if (id) {
+          setState(result);
+        } else {
+          setState(Object.values(result));
+        }
       })
       .finally(() => {
         setPending(false);
