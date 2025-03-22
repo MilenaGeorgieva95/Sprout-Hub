a360dd54835cdfe8a1ff4933e3c3ac30b557628f
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-22 06:40:15 +0000

    add requester to utils

---
+++ client/src/utils/requester.js
@@ -0,0 +1,45 @@
+const host = "http://localhost:3030";
+
+export async function requester(method, url, body, userCtx) {
+  const options = {
+    method,
+    headers: {},
+  };
+
+  if (body) {
+    options.headers["Content-Type"] = "application/json";
+    options.body = JSON.stringify(body);
+  }
+
+  const userData = userCtx;
+  if (userData && userData.accessToken) {
+    options.headers["X-Authorization"] = userData.accessToken;
+  }
+
+  try {
+    const res = await fetch(`${host}${url}`, options);
+    let data;
+    if (res.status !== 204) {
+      data = await res.json();
+    }
+
+    if (res.ok == false) {
+      if (res.status === 403) {
+        // clearUserData();
+      }
+      const error = data;
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    alert(`Error: ${error.message}`);
+    throw error;
+  }
+}
+
+export const request = {
+  get: requester.bind(null, "GET"),
+  post: requester.bind(null, "POST"),
+  put: requester.bind(null, "PUT"),
+  del: requester.bind(null, "DELETE"),
+};
