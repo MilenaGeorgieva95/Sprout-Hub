71eba5385429634b15361cd79f05df70af74f22f
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-03-22 10:59:53 +0000

    change posts details page  layout

--- client/src/components/forum/post-details/PostDetails.jsx
+++ client/src/components/forum/post-details/PostDetails.jsx
@@ -4,7 +4,56 @@ import { Link, useParams } from "react-router";
 
 import styles from "./PostDetails.module.css";
 import useFetch from "../../../hooks/usefetch";
+import { Radio, RadioGroup } from "@headlessui/react";
 
+const product = {
+  name: "Basic Tee 6-Pack",
+  price: "$192",
+  href: "#",
+  images: [
+    {
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
+      alt: "Two each of gray, white, and black shirts laying flat.",
+    },
+    {
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
+      alt: "Model wearing plain black basic tee.",
+    },
+    {
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
+      alt: "Model wearing plain gray basic tee.",
+    },
+    {
+      src: "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
+      alt: "Model wearing plain white basic tee.",
+    },
+  ],
+  colors: [
+    { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
+    { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
+    { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
+  ],
+  sizes: [
+    { name: "XXS", inStock: false },
+    { name: "XS", inStock: true },
+    { name: "S", inStock: true },
+    { name: "M", inStock: true },
+    { name: "L", inStock: true },
+    { name: "XL", inStock: true },
+    { name: "2XL", inStock: true },
+    { name: "3XL", inStock: true },
+  ],
+  description:
+    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+  highlights: [
+    "Hand cut and sewn locally",
+    "Dyed with our proprietary colors",
+    "Pre-washed & pre-shrunk",
+    "Ultra-soft 100% cotton",
+  ],
+  details:
+    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+};
 const reviews = { href: "#", average: 4, totalCount: 117 };
 
 function classNames(...classes) {
@@ -14,28 +63,58 @@ function classNames(...classes) {
 const baseUrl = "http://localhost:3030/jsonstore/forum/posts/";
 
 export default function PostDetails() {
+  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
+  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+
   const { postId } = useParams();
   const [pending, postData] = useFetch(baseUrl, {}, postId);
   console.log(postData);
 
   return (
-    <div className={"bg-white " + styles.mainContainer}>
+    <div className="bg-white">
       <div className="pt-6">
         {/* Image gallery */}
-        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
+        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
           <img
             alt={postData.title}
             src={postData.imgUrl}
             className="hidden size-full rounded-lg object-cover lg:block"
           />
+          <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+            <img
+              alt={postData.title}
+              src={postData.imgUrl}
+              className="aspect-3/2 w-full rounded-lg object-cover"
+            />
+            <img
+              alt={postData.title}
+              src={postData.imgUrl}
+              className="aspect-3/2 w-full rounded-lg object-cover"
+            />
+          </div>
+          <img
+            alt={postData.title}
+            src={postData.imgUrl}
+            className="aspect-4/5 size-full object-cover sm:rounded-lg lg:aspect-auto"
+          />
         </div>
 
         {/* Product info */}
-        <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:grid-rows-[auto_auto_1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
-          <div className="lg:col-span-3 lg:pr-8">
+        <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto_auto_1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
             <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
               {postData.title}
             </h1>
+          </div>
+
+          {/* Options */}
+          <div className="mt-4 lg:row-span-3 lg:mt-0">
+            <h2 className="sr-only">Product information</h2>
+            <p className="text-3xl tracking-tight text-gray-900">
+              {product.price}
+            </p>
+
+            {/* Reviews */}
             <div className="mt-6">
               <h3 className="sr-only">Reviews</h3>
               <div className="flex items-center">
@@ -62,19 +141,131 @@ export default function PostDetails() {
                 </Link>
               </div>
             </div>
+
+            <form className="mt-10">
+              {/* Colors */}
+              <div>
+                <h3 className="text-sm font-medium text-gray-900">Color</h3>
+
+                <fieldset aria-label="Choose a color" className="mt-4">
+                  <RadioGroup
+                    value={selectedColor}
+                    onChange={setSelectedColor}
+                    className="flex items-center gap-x-3"
+                  >
+                    {product.colors.map((color) => (
+                      <Radio
+                        key={color.name}
+                        value={color}
+                        aria-label={color.name}
+                        className={classNames(
+                          color.selectedClass,
+                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-hidden data-checked:ring-2 data-focus:data-checked:ring-3 data-focus:data-checked:ring-offset-1"
+                        )}
+                      >
+                        <span
+                          aria-hidden="true"
+                          className={classNames(
+                            color.class,
+                            "size-8 rounded-full border border-black/10"
+                          )}
+                        />
+                      </Radio>
+                    ))}
+                  </RadioGroup>
+                </fieldset>
+              </div>
+
+              {/* Sizes */}
+              <div className="mt-10">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-sm font-medium text-gray-900">Size</h3>
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Size guide
+                  </a>
+                </div>
+
+                <fieldset aria-label="Choose a size" className="mt-4">
+                  <RadioGroup
+                    value={selectedSize}
+                    onChange={setSelectedSize}
+                    className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4"
+                  >
+                    {product.sizes.map((size) => (
+                      <Radio
+                        key={size.name}
+                        value={size}
+                        disabled={!size.inStock}
+                        className={classNames(
+                          size.inStock
+                            ? "cursor-pointer bg-white text-gray-900 shadow-xs"
+                            : "cursor-not-allowed bg-gray-50 text-gray-200",
+                          "group relative flex items-center justify-center rounded-md border px-4 py-3 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-hidden data-focus:ring-2 data-focus:ring-indigo-500 sm:flex-1 sm:py-6"
+                        )}
+                      >
+                        <span>{size.name}</span>
+                        {size.inStock ? (
+                          <span
+                            aria-hidden="true"
+                            className="pointer-events-none absolute -inset-px rounded-md border-2 border-transparent group-data-checked:border-indigo-500 group-data-focus:border"
+                          />
+                        ) : (
+                          <span
+                            aria-hidden="true"
+                            className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
+                          >
+                            <svg
+                              stroke="currentColor"
+                              viewBox="0 0 100 100"
+                              preserveAspectRatio="none"
+                              className="absolute inset-0 size-full stroke-2 text-gray-200"
+                            >
+                              <line
+                                x1={0}
+                                x2={100}
+                                y1={100}
+                                y2={0}
+                                vectorEffect="non-scaling-stroke"
+                              />
+                            </svg>
+                          </span>
+                        )}
+                      </Radio>
+                    ))}
+                  </RadioGroup>
+                </fieldset>
+              </div>
+
+              <button
+                type="submit"
+                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
+              >
+                Add to bag
+              </button>
+            </form>
           </div>
 
-          <div className="py-10 lg:col-span-3 lg:col-start-1  lg:pt-6 lg:pr-8 lg:pb-16">
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pr-8 lg:pb-16">
             {/* Description and details */}
             <div>
               <h3 className="sr-only">Description</h3>
 
               <div className="space-y-6">
-                <p className="text-base text-gray-900">{postData.text}</p>
+                <p className="text-base text-gray-900">{product.description}</p>
               </div>
             </div>
 
             <div className="mt-10">
+              <div className="mt-10">
+                <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+                <div className="mt-4 space-y-6">
+                  <p className="text-sm text-gray-600">{product.details}</p>
+                </div>
+              </div>
               <div className="mt-4 space-b">
                 <Link
                   to={`/posts/${postData._id}/edit`}
