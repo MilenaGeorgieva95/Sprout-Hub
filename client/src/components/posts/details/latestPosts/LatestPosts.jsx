4e000b022e6b7b049f07f971f36d1856892a46ec
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-05-01 13:14:48 +0100

    fix import name

--- client/src/components/posts/details/latestPosts/LatestPosts.jsx
+++ client/src/components/posts/details/latestPosts/LatestPosts.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router";
 import { useLatestPosts } from "../../../../api/postsApi";
-import PostCard from "./postCard";
+import PostCard from "./PostCard";
 
 export default function LatestPosts() {
   const { postId } = useParams();
